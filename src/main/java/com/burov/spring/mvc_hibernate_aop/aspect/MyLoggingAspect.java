305f3453086487b718a79c733e764a58e10aed90
burov-a-s <artem.burov1988@yandex.ru>
2021-09-27 21:42:40 +0300

    Init commit

---
+++ src/main/java/com/burov/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -0,0 +1,24 @@
+package com.burov.spring.mvc_hibernate_aop.aspect;
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+
+    @Around("execution(* com.burov.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+
+        System.out.println("Method " + methodName + " begin");
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        System.out.println("Method " + methodName + " end");
+        return targetMethodResult;
+    }
+}
---
+++ src/main/java/com/burov/spring/mvc_hibernate_aop/controllers/MyController.java
@@ -0,0 +1,51 @@
+package com.burov.spring.mvc_hibernate_aop.controllers;
+
+import com.burov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import com.burov.spring.mvc_hibernate_aop.entity.Employee;
+import com.burov.spring.mvc_hibernate_aop.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class MyController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/")
+    public String showAllEmployees(Model model) {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        model.addAttribute("allEmps", allEmployees);
+        return "all-employees";
+    }
+
+    @GetMapping("/addNewEmployee")
+    public String addNewEmployee(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @GetMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model) {
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @GetMapping("deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId") int id) {
+        employeeService.deleteEmployee(id);
+        return "redirect:/";
+    }
+}
